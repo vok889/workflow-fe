4122591d9dcdc94e4e32a88773d7fe429ed3b712
Jame Hanchai <vok889@gmail.com>
2024-09-07 13:25:41 +0700

    version2.5

--- src/Greeting.tsx
+++ src/Greeting.tsx
@@ -3,11 +3,13 @@ type greetingParams = {
     age: number;
     format?: 'Table' | 'Default';
   }
+  
   function greeting({name, age, format}: greetingParams) {
     if (format == "Table" ){
-      const result = {};
-      result.name = name;
-      result.age = age;
+      let result: greetingParams = {
+        name: name,
+        age: age
+    };
       console.table(result)
     } else if (format == "Default" || format == null ){
       console.log("Hi, " + name)
