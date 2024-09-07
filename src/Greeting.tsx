3d7092b0c256bfc286c4d849f491a239936d1c80
Jame Hanchai <vok889@gmail.com>
2024-09-07 12:05:44 +0700

    version2.1

--- src/Greeting.tsx
+++ src/Greeting.tsx
@@ -1,11 +1,16 @@
-function greeting(props:{name: string, age: number, format: string}) {
-    if (props.format == "Table" ){
+type greeting = {
+    name: string;
+    age: number;
+    format: 'Table' | 'Default';
+  }
+  
+  function greeting({name, age, format}: greeting) {
+    if (format == "Table" ){
       const result = {};
-      result.name = props.name;
-      result.age = props.age;
+      result.name = name;
+      result.age = age;
       console.table(result )
     } else{
-      console.log("Hi, " + props.name)
-    }
-    
+      console.log("Hi, " + name)
+    } 
   }
